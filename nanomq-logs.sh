14d87b5d1b26a0f521ef60f488ea369b4eac53cd
Kasper Nielsen <kasperni@gmail.com>
2025-08-09 11:36:25 +0200

    cleanup

--- nanomq-logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script to view NanoMQ debug logs
-
-echo "=== NanoMQ Debug Logs ==="
-docker compose -f docker/local/docker-compose.yml logs nanomq --tail ${1:-50}
--- test-mqtt-subscribe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Test MQTT subscription using mosquitto_sub
-
-echo "Subscribing to /sync topic..."
-mosquitto_sub -h localhost -p 1883 -u api -P mqtt_api_password -t /sync -v
